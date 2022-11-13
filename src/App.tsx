e2a651b9a8682ca9a4c90817756e5a6ecf525517
Joalisson Gomes <joalissongomes.webdev@gmail.com>
2022-11-13 16:27:15 -0300

    feat: Configuring the theme and adding type to it

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
   return (
-    <div className="App">
-     <h1>Hello, Ignite Timer</h1>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello Ignite Timer</h1>
+    </ThemeProvider>
   )
 }
