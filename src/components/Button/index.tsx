71550da370435e11857e9840303781509c583ac8
Joalisson Gomes <joalissongomes.webdev@gmail.com>
2022-11-13 17:28:47 -0300

    feat: Delete Button component

--- src/components/Button/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, ButtonVariant } from './styles'
-
-interface ButtonContainerProps {
-  variant?: ButtonVariant
-}
-
-export function Button({ variant = 'primary' }: ButtonContainerProps) {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
-}
